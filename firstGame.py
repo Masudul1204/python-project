3802ceee3b366a53b7d8f51881b80612c8ec9864
Masudul1204 <nmasudul@gmail.com>
2025-01-19 05:08:09 +0300

    1st Project

---
+++ firstGame.py
@@ -0,0 +1,8 @@
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter a 2nd number: '))
+
+def add_two_number(num1, num2):
+    return num1+num2
+
+x = add_two_number(num1,num2)
+print('After add the new number: ', x)
